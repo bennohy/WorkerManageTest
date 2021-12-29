2c2eb87a1f5489bf9d5717ba56b3ed79af2eb83f
bennohy <24535615+bennohy@users.noreply.github.com>
2021-12-29 09:50:52 +0800

    modify content resolver query

--- app/src/main/java/com/evermore/benno/workermanagetest/MediaContentObserver.kt
+++ app/src/main/java/com/evermore/benno/workermanagetest/MediaContentObserver.kt
@@ -16,23 +16,13 @@ class MediaContentObserver(private val context: Context, private val contentUri:
     override fun onChange(selfChange: Boolean) {
         super.onChange(selfChange)
         Log.d("ContentChange", "change selfChange > $selfChange")
+        queryChangeUri(contentUri)
     }
 
-    override fun onChange(selfChange: Boolean, uri: Uri?) {
-        super.onChange(selfChange, uri)
-        Log.d("ContentChange", "change selfChange > $selfChange, uri > $uri")
-    }
-
-    override fun onChange(selfChange: Boolean, uri: Uri?, flags: Int) {
-        super.onChange(selfChange, uri, flags)
-        Log.d("ContentChange", "change selfChange > $selfChange, uri > $uri, flags > $flags")
-        val flag = when (flags) {
-            NOTIFY_INSERT -> "NOTIFY_INSERT"
-            NOTIFY_UPDATE -> "NOTIFY_UPDATE"
-            NOTIFY_DELETE -> "NOTIFY_DELETE"
-            else -> flags
-        }
-        Log.d("ContentChange", "->change flags > $flag")
+    private fun queryChangeUri(uri: Uri) {
+        Log.d("ContentChange", "->queryChangeUri > $uri")
+        val queryLimit = 1
+        val querySortBy = MediaStore.Images.Media._ID
         val projection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             arrayOf(
                 MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA, MediaStore.Images.Media.TITLE,
@@ -46,47 +36,50 @@ class MediaContentObserver(private val context: Context, private val contentUri:
         }
         val selection = "${MediaStore.Images.Media.TITLE} NOT LIKE ?"
         val selectionArgs = arrayOf(".pending%")
-        uri?.run {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                context.contentResolver.query(this, projection, Bundle().apply {
-                    putInt(QUERY_ARG_LIMIT, 1)
-                    putStringArray(QUERY_ARG_SORT_COLUMNS, arrayOf(MediaStore.Images.Media._ID))
-                    putInt(QUERY_ARG_SORT_DIRECTION, QUERY_SORT_DIRECTION_DESCENDING)
-//                    putString(QUERY_ARG_SQL_SELECTION, selection)
-//                    putStringArray(QUERY_ARG_SQL_SELECTION_ARGS, selectionArgs)
-                }, null)
-            } else {
-                context.contentResolver.query(this, projection, null, null, "${MediaStore.Images.Media._ID} DESC LIMIT 1")
-            }?.use { cursor ->
-                Log.d("ContentChange", "-->change uri query cursor.count > ${cursor.count}")
-                if (cursor.count > 0) {
-                    if (cursor.moveToFirst()) {
-                        do {
-                            val idColumn = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                            val dataColumn = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
-                            val titleColumn = cursor.getColumnIndex(MediaStore.Images.Media.TITLE)
-                            val displayNameColumn = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
-                            val id = cursor.getString(idColumn)
-                            val data = cursor.getString(dataColumn)
-                            val relativePath = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                                val relativePathColumn = cursor.getColumnIndex(MediaStore.Images.Media.RELATIVE_PATH)
-                                cursor.getString(relativePathColumn)
-                            } else {
-                                ""
-                            }
-                            val title = cursor.getString(titleColumn)
-                            val displayName = cursor.getString(displayNameColumn)
-                            Log.d("ContentChange", "--->uri cursor of id > $id")
-                            Log.d("ContentChange", "--->uri cursor of data > $data")
-                            Log.d("ContentChange", "--->uri cursor of relativePath > $relativePath")
-                            Log.d("ContentChange", "--->uri cursor of title > $title")
-                            Log.d("ContentChange", "--->uri cursor of displayName > $displayName")
-                        } while (cursor.moveToNext())
-                    }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            context.contentResolver.query(uri, projection, Bundle().apply {
+                putInt(QUERY_ARG_LIMIT, queryLimit)
+                putStringArray(QUERY_ARG_SORT_COLUMNS, arrayOf(querySortBy))
+                putInt(QUERY_ARG_SORT_DIRECTION, QUERY_SORT_DIRECTION_DESCENDING)
+                    putString(QUERY_ARG_SQL_SELECTION, selection)
+                    putStringArray(QUERY_ARG_SQL_SELECTION_ARGS, selectionArgs)
+            }, null)
+        } else {
+            context.contentResolver.query(uri, projection, null, null, "$querySortBy DESC LIMIT $queryLimit")
+        }?.use { cursor ->
+            Log.d("ContentChange", "-->change uri query cursor.count > ${cursor.count}")
+            if (cursor.count > 0) {
+                if (cursor.moveToFirst()) {
+                    do {
+                        val idColumn = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                        val dataColumn = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+                        val titleColumn = cursor.getColumnIndex(MediaStore.Images.Media.TITLE)
+                        val displayNameColumn = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
+                        val dateModifiedColumn = cursor.getColumnIndex(MediaStore.Images.Media.DATE_MODIFIED)
+                        val dateAddedColumn = cursor.getColumnIndex(MediaStore.Images.Media.DATE_ADDED)
+                        val id = cursor.getString(idColumn)
+                        val data = cursor.getString(dataColumn)
+                        val relativePath = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            val relativePathColumn = cursor.getColumnIndex(MediaStore.Images.Media.RELATIVE_PATH)
+                            cursor.getString(relativePathColumn)
+                        } else {
+                            ""
+                        }
+                        val title = cursor.getString(titleColumn)
+                        val displayName = cursor.getString(displayNameColumn)
+                        val dateModified = cursor.getString(dateModifiedColumn)
+                        val dateAdded = cursor.getString(dateAddedColumn)
+                        Log.d("ContentChange", "--->uri cursor of id > $id")
+                        Log.d("ContentChange", "--->uri cursor of data > $data")
+                        Log.d("ContentChange", "--->uri cursor of relativePath > $relativePath")
+                        Log.d("ContentChange", "--->uri cursor of title > $title")
+                        Log.d("ContentChange", "--->uri cursor of displayName > $displayName")
+                    } while (cursor.moveToNext())
                 }
-                cursor.close()
-                Log.d("ContentChange", "uri cursor close")
             }
+            cursor.close()
+            Log.d("ContentChange", "uri cursor close")
         }
     }
 }
